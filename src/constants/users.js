acfacf0e0fba49e0c9ca7d36005a87068eb80d95
Shruti Kapoor <shruti.kapoor08@gmail.com>
2018-04-01 16:57:15 -0400

    added users.js

---
+++ src/constants/users.js
@@ -0,0 +1,14 @@
+const USERS = [
+  {
+    id: 234,
+    name: 'Laura Smith',
+    recentlyMessaged: [456]
+  },
+  {
+    id: 456,
+    name: 'Bob Handler',
+    recentlyMessaged: [234]
+  }
+];
+
+export default USERS;
